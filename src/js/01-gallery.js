19ec95f53be1ea37c7c8b4edc358d914bbf5dea1
Petro Sobchuk <sobchuk.p@gmail.com>
2023-01-17 18:17:49 +0200

    Change file gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,6 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
-import simpleLightbox from 'simplelightbox';
 // Change code below this line
 
 console.log(simpleLightbox);
